ab3e15312b75c12a87a0b5ccb41d3953e45262bb
Theophile-Kango <fadhili.kango@gmail.com>
2020-07-31 13:27:20 +0200

    Add the state to BooksForm

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,20 +1,30 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
-import React from 'react';
+import React, {Component} from 'react';
 
-export default function BooksForm() {
-  const options = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  return (
-    <form>
-      <label htmlFor="title">Book Title</label>
-      <input id="title" name="title" type="text" placeholder="book title" />
-      <br />
-      <label htmlFor="category">Categories</label>
-      <select id="category">
-        {options.map(option => (<option key={option}>{option}</option>))}
-      </select>
-      <br />
-      <button type="submit">submit</button>
-    </form>
-  );
+export default class BooksForm extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      title: '',
+      category: ''
+    }
+  }
+  
+  render(){
+    const options = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+    return (
+      <form>
+        <label htmlFor="title">Book Title</label>
+        <input id="title" name="title" type="text" placeholder="book title" />
+        <br />
+        <label htmlFor="category">Categories</label>
+        <select id="category">
+          {options.map(option => (<option key={option}>{option}</option>))}
+        </select>
+        <br />
+        <button type="submit">submit</button>
+      </form>
+    );
+  }
 }
