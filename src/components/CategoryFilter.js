cf1febf277e93d8c2d43c906ad6dd47d50aa6430
Theophile-Kango <fadhili.kango@gmail.com>
2020-08-03 13:18:35 +0200

    Changed the default font-familly

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,6 +1,17 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+const Categories = styled.h3`
+  color: #f5f6fa;
+  text-transform: uppercase;
+  font-size: 16px;;
+`;
+
+const Title = styled.h1`
+  color: #0290ff;
+  font-size: 37px;
+`;
 export default function CategoryFilter({ handleFilterChange }) {
   const options = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   const filterChange = useCallback(event => {
@@ -8,7 +19,9 @@ export default function CategoryFilter({ handleFilterChange }) {
   }, [handleFilterChange]);
   return (
     <>
-      <span>Filter by</span>
+      <Title>Bookstore CMS</Title>
+      <Categories style={{ color: '#121212', }}>books</Categories>
+      <Categories>categories</Categories>
       <select onChange={filterChange}>
         {options.map(option => (<option key={option}>{option}</option>))}
       </select>
